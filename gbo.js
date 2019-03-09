486c08a34e25b8e836dfad84b13baf9dbc157575
nina-barraco <barraconina@gmail.com>
2019-03-09 16:04:47 -0500

    updated animations and scroll

--- gbo.js
+++ gbo.js
@@ -2,3 +2,17 @@ function clickNav(x) {
     var display = document.getElementById("topnav-mobile-js");
     display.classList.toggle("display-topnav-mobile");
   }
+
+function fadeIn(x) {
+  var fadeElements = Array.from(document.getElementsByClassName("fade-js"));
+  var windowHeight = window.innerHeight;
+  function checkPosition() {
+    for (var i = 0; i < fadeElements.length; i++) {
+      var positionFromTop = fadeElements[i].getBoundingClientRect().top;
+      if (positionFromTop - windowHeight <= 0) {
+        fadeElements[i].classList.add("fade-animation");
+      }
+    }
+  };
+  checkPosition();
+}
